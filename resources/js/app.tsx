242fe00efdd346a5575dbdc171f4a083bd11cad4
Rafael906 <erputare2024@incos-santacruz.edu.bo>
2026-05-08 20:19:56 -0400

    instalacion de react con tyscript

---
+++ resources/js/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <div>
+            <h1>React + TypeScript funcionando</h1>
+        </div>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
